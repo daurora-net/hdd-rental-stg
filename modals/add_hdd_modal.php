f22436c4b5593f36f3cf4b25e9a34fd3390e6cd9
daurora-net <your_email@example.com>
2025-01-23 17:15:01 +0900

    add_hdd_modal修正

--- modals/add_hdd_modal.php
+++ modals/add_hdd_modal.php
@@ -1,10 +1,16 @@
 <div id="addHddModal" class="modal">
-    <div class="modal-content">
-        <span class="close" onclick="document.getElementById('addHddModal').style.display='none'">&times;</span>
-        <form method="post" action="actions/add_hdd.php">
-            <label for="hddName">HDD名称:</label>
-            <input type="text" id="hddName" name="hddName" required>
-            <button type="submit">追加</button>
-        </form>
-    </div>
+  <div class="modal-content">
+    <span class="close" onclick="document.getElementById('addHddModal').style.display='none'">&times;</span>
+    <form method="post" action="actions/add_hdd.php">
+      <h3>HDD追加</h3>
+
+      <div class="flex">
+        <div class="form-content w-300px">
+          <label for="hddName" class="required">HDD名</label>
+          <input type="text" id="hddName" name="hddName" required>
+        </div>
+      </div>
+      <button type="submit" class="modal-btn">保存</button>
+    </form>
+  </div>
 </div>
